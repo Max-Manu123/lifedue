36c6273362b47300516f9c92fc5b87470a79c0ec
Max-Manu123 <repmanuel77@gmail.com>
2026-09-22 10:59:58 +0100

    Add LifeDue PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'lifedue-shell-v1'
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['/','/manifest.webmanifest']))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+
+  const url = new URL(event.request.url)
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then(cache => cache.put('/', copy))
+          return response
+        })
+        .catch(() => caches.match('/'))
+    )
+    return
+  }
+
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+        if (response.ok) {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy))
+        }
+        return response
+      }))
+    )
+  }
+})
